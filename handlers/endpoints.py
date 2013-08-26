19fccba6576936907d5ff9b052edacea53a39d09
Ashray <ashray@b-eagles.com>
2013-08-26 05:11:52 -0400

    more updates

--- handlers/endpoints.py
+++ handlers/endpoints.py
@@ -19,11 +19,31 @@ class ScorePreferenceTrends(WebRequestHandler):
 		path = 'endpoints/aggregate_campaigns/score_preference_trends.html'
 		self.response.out.write(self.get_rendered_html(path, dict()))
 
+class BidsTrends(WebRequestHandler):
+	def get(self):
+		path = 'endpoints/aggregate_deals/bids_trends.html'
+		self.response.out.write(self.get_rendered_html(path, dict()))
+
+class UserTrends(WebRequestHandler):
+	def get(self):
+		path = 'endpoints/aggregate_deals/user_trends.html'
+		self.response.out.write(self.get_rendered_html(path, dict()))
+
+class TimeTrends(WebRequestHandler):
+	def get(self):
+		path = 'endpoints/aggregate_deals/time_trends.html'
+		self.response.out.write(self.get_rendered_html(path, dict()))
+
+class AverageSpending(WebRequestHandler):
+	def get(self):
+		path = 'endpoints/aggregate_deals/average_spending.html'
+		self.response.out.write(self.get_rendered_html(path, dict()))
+
 app = webapp2.WSGIApplication([('/endpoints', Endpoints),
    ('/endpoints/aggregate_campaigns/discount_trends', DiscountTrends), 
-   ('/endpoints/aggregate_campaigns/score_preference_trends', ScorePreferenceTrends)
-   #('/aggregate_deals/bids_trends', BidsTrends),
-   #('/aggregate_deals/user_trends', UserTrends), 
-   #('/aggregate_deals/time_trends', TimeTrends), 
-   #('/aggregate_deals/average_spending', AverageSpending)
+   ('/endpoints/aggregate_campaigns/score_preference_trends', ScorePreferenceTrends),
+   ('/endpoints/aggregate_deals/bids_trends', BidsTrends),
+   ('/endpoints/aggregate_deals/user_trends', UserTrends),
+   ('/endpoints/aggregate_deals/time_trends', TimeTrends), 
+   ('/endpoints/aggregate_deals/average_spending', AverageSpending)
    ])
