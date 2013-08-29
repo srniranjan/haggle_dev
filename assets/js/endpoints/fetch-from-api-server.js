5dfc31718a4da701733944e369e3959fb6b27520
Ashray <ashray@b-eagles.com>
2013-08-29 03:07:51 -0400

    added loading indicator

--- assets/js/endpoints/fetch-from-api-server.js
+++ assets/js/endpoints/fetch-from-api-server.js
@@ -3,9 +3,13 @@ $('#params').submit(function(){
 		type: "POST",
 		url: "/fetch_from_api_server",
 		data: $("#params").serialize(),
+		beforeSend: function(){
+			$('#loading_indicator').show();
+		},
 		success: function(data){
 			var pretty_data = JSON.stringify(JSON.parse(data), undefined, 4);
 			$('#response').html(pretty_data);
+			$('#loading_indicator').hide();
 		}
 	});
 	return false;
