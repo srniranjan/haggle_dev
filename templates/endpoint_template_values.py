df65a9e3b556e6918a6a52a421a1f5b17665d034
Ashray <ashray@b-eagles.com>
2013-08-29 02:08:10 -0400

    added all working endpoints' template values

--- templates/endpoint_template_values.py
+++ templates/endpoint_template_values.py
@@ -1,7 +1,27 @@
 TEMPLATE_VALUES = {
         'discount_trends': {
             'header': 'Discount Trends',
-            'params': { 'endpoint': 'aggregate_campaigns', 'property': 'discount_trends', 'lat': 12 , 'lon': 123, 'radius': 1000 },
+            'params': { 'endpoint': 'aggregate_campaigns', 'property': 'discount_trends', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
+            },
+        'score_preference_trends': {
+            'header': 'Score Preference Trends',
+            'params': { 'endpoint': 'aggregate_campaigns', 'property': 'score_preference_trends', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
+            },
+        'bids_trends': {
+            'header': 'Bids Trends',
+            'params': { 'endpoint': 'aggregate_deals', 'property': 'bids_trends', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
+            },
+        'user_trends': {
+            'header': 'User Trends',
+            'params': { 'endpoint': 'aggregate_deals', 'property': 'user_trends', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
+            },
+        'time_trends': {
+            'header': 'Time Trends',
+            'params': { 'endpoint': 'aggregate_deals', 'property': 'time_trends', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
+            },
+        'average_spending': {
+            'header': 'Average Spending',
+            'params': { 'endpoint': 'aggregate_deals', 'property': 'average_spending', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
             }
         }
 
