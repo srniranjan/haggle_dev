71b7836e1e5e60a8fa07412dae6812ea9de225d5
Ashray <ashray@b-eagles.com>
2013-09-11 03:03:51 -0400

    added description to api endpoints

--- templates/endpoint_template_values.py
+++ templates/endpoint_template_values.py
@@ -1,31 +1,37 @@
 TEMPLATE_VALUES = {
         'discount_trends': {
             'header': 'Discount Trends',
+            'description': 'Placeholder description for Discount Trends',
             'endpoint': 'aggregate_campaigns',
             'params': { 'property': 'discount_trends', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
             },
         'score_preference_trends': {
             'header': 'Score Preference Trends',
+            'description': 'Placeholder description for Score Preference Trends',
             'endpoint': 'aggregate_campaigns',
             'params': { 'property': 'score_preference_trends', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
             },
         'bids_trends': {
             'header': 'Bids Trends',
+            'description': 'Placeholder description for Bids Trends',
             'endpoint': 'aggregate_deals',
             'params': { 'property': 'bids_trends', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
             },
         'user_trends': {
             'header': 'User Trends',
+            'description': 'Placeholder description for User Trends',
             'endpoint': 'aggregate_deals',
             'params': { 'property': 'user_trends', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
             },
         'time_trends': {
             'header': 'Time Trends',
+            'description': 'Placeholder description for Time Trends',
             'endpoint': 'aggregate_deals',
             'params': { 'property': 'time_trends', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
             },
         'average_spending': {
             'header': 'Average Spending',
+            'description': 'Placeholder description for Average Spending',
             'endpoint': 'aggregate_deals',
             'params': { 'property': 'average_spending', 'lat': 40.745619, 'lon': -73.985296, 'radius': 3000 }
             }
