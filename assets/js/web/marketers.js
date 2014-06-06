acf5cfba733d40ffffec2932fe192dbdb70b96ae
Ranju <ranju@b-eagles.com>
2014-06-06 22:52:20 +0530

    Changed the age ranges for the donut chart

--- assets/js/web/marketers.js
+++ assets/js/web/marketers.js
@@ -44,7 +44,10 @@ $(document).ready(function(){
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    data = [{age:'<5',population:2704659},{age:'5-13',population:4499890},{age:'14-17',population:2159981},{age:'18-24',population:3853788},{age:'25-44',population:14106543},{age:'45-64',population:8819342},{age:'≥65',population:612463}];
+    data = [{age:'18-25',population:30},
+            {age:'25-30',population:40},
+            {age:'30-32',population:20},
+            {age:'32-40',population:10}];
     data.forEach(function(d) {
     d.population = +d.population;
     });
